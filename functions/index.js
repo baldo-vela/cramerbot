8ec6a883764e6f5fe29748e883b85eef121d0330
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-01-24 16:01:11 -0600

    Prototypes the API config. Still needs keys

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
 const functions = require('firebase-functions');
 
 /// SDK Config ///
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({ 
+    organization: functions.config().openai.id,  //API ID key
+    apiKey: functions.config().openai.key,       //API key key
+});
 
+// Initalize the OpenAI 
+const openai = new OpenAIApi(configuration);
 
 /// Exports ///
