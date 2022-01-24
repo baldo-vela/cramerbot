5e8dc1d976e140c919b4759641ec77e458e6f8f8
baldo-vela <74266847+baldo-vela@users.noreply.github.com>
2022-01-24 16:10:07 -0600

    Initializes https function for testing

--- functions/index.js
+++ functions/index.js
@@ -11,3 +11,6 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 /// Exports ///
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
